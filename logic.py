a5d2f9a7fcf9c3c9d6d985d6e75c13741722bd9c
toma1235 <tomasrocket17@gmail.com>
2025-03-25 17:12:16 +0100

    Final bot

--- logic.py
+++ logic.py
@@ -90,12 +90,31 @@ class DatabaseManager:
             return cursor.execute("SELECT * FROM prizes WHERE used = 0 ORDER BY random()").fetchall()[0]
             
   
-def hide_img(img_name):
-    image = cv2.imread(f'img/{img_name}')
-    blurred_image = cv2.GaussianBlur(image, (15, 15), 0)
-    pixelated_image = cv2.resize(blurred_image, (30, 30), interpolation=cv2.INTER_NEAREST)
-    pixelated_image = cv2.resize(pixelated_image, (image.shape[1], image.shape[0]), interpolation=cv2.INTER_NEAREST)
-    cv2.imwrite(f'hidden_img/{img_name}', pixelated_image)
+    def hide_img(img_name):
+        image = cv2.imread(f'img/{img_name}')
+        blurred_image = cv2.GaussianBlur(image, (15, 15), 0)
+        pixelated_image = cv2.resize(blurred_image, (30, 30), interpolation=cv2.INTER_NEAREST)
+        pixelated_image = cv2.resize(pixelated_image, (image.shape[1], image.shape[0]), interpolation=cv2.INTER_NEAREST)
+        cv2.imwrite(f'hidden_img/{img_name}', pixelated_image)
+
+
+    def get_winners_count(self, prize_id):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('SELECT COUNT(*) FROM winners WHERE prize_id = ?', (prize_id, ))
+            return cur.fetchall()[0][0]
+    
+    
+        
+    def get_rating(self):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('''
+    SELECT users.user_name, COUNT(winners.prize_id) as count_prize FROM winners INNER JOIN users ON users.user_id = winners.user_id GROUP BY winners.user_id ORDER BY count_prize LIMIT 10
+    ''')
+        return cur.fetchall()
 
 if __name__ == '__main__':
     manager = DatabaseManager(DATABASE)
@@ -103,3 +122,4 @@ if __name__ == '__main__':
     prizes_img = os.listdir('img')
     data = [(x,) for x in prizes_img]
     manager.add_prize(data)
+    
